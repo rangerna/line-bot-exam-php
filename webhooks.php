529a0c5cc1a91007841335354ced490094067601
rangerna <43648433+rangerna@users.noreply.github.com>
2021-07-16 10:22:26 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,6 +1,6 @@
 <?php
 require "vendor/autoload.php";
-$access_token = '66FwOAiMBIUDM9tE2fITiU+cSnxlL7PFILbP0bAWfO4yvkepV7EzoAasRzqzA8hoa3gM/cmOLaBzSjTnil3TvbX/gYXsHZBGV1JMfhpT26a2x6ByttzgmWJ06Ic0Pl4bPfklThvN3ikB6y0J9EWNggdB04t89/1O/w1cDnyilFU=';
+$access_token = 'GEWRF0Kaz+Dur3IwPEsrHjaVmdNt7Jn8FI/jTkPv5Xu9a9yT+Fy/ht9QDEfrvRm+a3gM/cmOLaBzSjTnil3TvbX/gYXsHZBGV1JMfhpT26beEhWvdznO2lhPfqPuYbPtqssCfOTZgZ1bWbZ3oBvCWgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '3396ea6e7653529878051838afd9c61c';
 $idPush = 'Uafe78d7208e99890028086e61318f46c'
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
