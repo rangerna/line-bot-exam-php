37900f22a614f0a30c7a71f0e783a40f745d5052
rangerna <43648433+rangerna@users.noreply.github.com>
2021-07-16 10:19:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,52 +1,10 @@
-<?php // callback.php
-
+<?php
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
 $access_token = '66FwOAiMBIUDM9tE2fITiU+cSnxlL7PFILbP0bAWfO4yvkepV7EzoAasRzqzA8hoa3gM/cmOLaBzSjTnil3TvbX/gYXsHZBGV1JMfhpT26a2x6ByttzgmWJ06Ic0Pl4bPfklThvN3ikB6y0J9EWNggdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			//$text = $event['source']['userId'];
-			$text = "สวัสดี";
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-echo "OK";
+$channelSecret = '3396ea6e7653529878051838afd9c61c';
+$idPush = 'Uafe78d7208e99890028086e61318f46c'
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
